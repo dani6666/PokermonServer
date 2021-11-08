e50ac07aed97ac83d1fc79eaaa773d7350bef6f9
Daniel <danielwz99@gmail.com>
2021-11-08 22:12:04 +0100

    fixing ports assignment

--- src/Pokermon/Program.cs
+++ src/Pokermon/Program.cs
@@ -1,11 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Pokermon
 {
@@ -21,6 +15,7 @@ namespace Pokermon
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://localhost:2137");
                 });
     }
 }
