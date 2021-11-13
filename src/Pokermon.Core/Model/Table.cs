a51a76b9f1f8a952855c82fb362ecb45067d5f6b
Daniel <danielwz99@gmail.com>
2021-11-14 00:45:27 +0100

    minor table endpoint changes

--- src/Pokermon.Core/Model/Table.cs
+++ src/Pokermon.Core/Model/Table.cs
@@ -4,6 +4,6 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int StartingBet { get; set; }
+        public int Players { get; set; }
     }
 }
